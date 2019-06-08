15c919ad2e1dd911d0ec8f047acb2c31de3ee135
ZhengPengqiao <157510312@qq.com>
2019-06-08 11:39:14 +0800

    add 添加滤波中中值,双边滤波,形态学图像处理,漫水填充

--- 306_滤波/main.cpp
+++ 306_滤波/main.cpp
@@ -1,97 +1,117 @@
-//---------------------------------【头文件、命名空间包含部分】-------------------------------
-//		描述：包含程序所使用的头文件和命名空间
-//------------------------------------------------------------------------------------------------
+//-----------------------------------【头文件包含部分】---------------------------------------
+//		描述：包含程序所依赖的头文件
+//----------------------------------------------------------------------------------------------
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
+//-----------------------------------【命名空间声明部分】---------------------------------------
+//		描述：包含程序所使用的命名空间
+//-----------------------------------------------------------------------------------------------
 using namespace std;
 using namespace cv;
 
 
 //-----------------------------------【全局变量声明部分】--------------------------------------
-//	描述：全局变量声明
+//		描述：全局变量声明
 //-----------------------------------------------------------------------------------------------
-Mat g_srcImage,g_dstImage1,g_dstImage2,g_dstImage3;//存储图片的Mat类型
-int g_nBoxFilterValue=3;  //方框滤波参数值
-int g_nMeanBlurValue=3;  //均值滤波参数值
-int g_nGaussianBlurValue=3;  //高斯滤波参数值
+Mat g_srcImage,g_dstImage1,g_dstImage2,g_dstImage3,g_dstImage4,g_dstImage5;
+int g_nBoxFilterValue=6;  //方框滤波内核值
+int g_nMeanBlurValue=10;  //均值滤波内核值
+int g_nGaussianBlurValue=6;  //高斯滤波内核值
+int g_nMedianBlurValue=10;  //中值滤波参数值
+int g_nBilateralFilterValue=10;  //双边滤波参数值
 
 
 //-----------------------------------【全局函数声明部分】--------------------------------------
-//	描述：全局函数声明
+//		描述：全局函数声明
 //-----------------------------------------------------------------------------------------------
-//四个轨迹条的回调函数
-static void on_BoxFilter(int, void *);		//均值滤波
-static void on_MeanBlur(int, void *);		//均值滤波
-static void on_GaussianBlur(int, void *);			//高斯滤波
+//轨迹条回调函数
+static void on_BoxFilter(int, void *);		//方框滤波
+static void on_MeanBlur(int, void *);		//均值块滤波器
+static void on_GaussianBlur(int, void *);			//高斯滤波器
+static void on_MedianBlur(int, void *);			//中值滤波器
+static void on_BilateralFilter(int, void *);			//双边滤波器
 void ShowHelpText();
 
 
 //-----------------------------------【main( )函数】--------------------------------------------
-//	描述：控制台应用程序的入口函数，我们的程序从这里开始
+//		描述：控制台应用程序的入口函数，我们的程序从这里开始
 //-----------------------------------------------------------------------------------------------
 int main(   )
 {
-    //改变console字体颜色
-    system("color 5F");
-
-    //输出帮助文字
-    ShowHelpText();
-
     // 载入原图
-    g_srcImage = imread( "../assert/image4.jpg", 1 );
-    if( !g_srcImage.data ) { printf("Oh，no，读取srcImage错误~！ \n"); return false; }
+    g_srcImage = imread( "../assert/image6.jpg", 1 );
+    if( !g_srcImage.data ) { printf("读取srcImage错误~！ \n"); return false; }
 
-    //克隆原图到三个Mat类型中
+    //克隆原图到四个Mat类型中
     g_dstImage1 = g_srcImage.clone( );
     g_dstImage2 = g_srcImage.clone( );
     g_dstImage3 = g_srcImage.clone( );
+    g_dstImage4 = g_srcImage.clone( );
+    g_dstImage5 = g_srcImage.clone( );
 
     //显示原图
     namedWindow("【<0>原图窗口】", 1);
     imshow("【<0>原图窗口】",g_srcImage);
 
 
-    //=================【<1>方框滤波】==================
+    //=================【<1>方框滤波】=========================
     //创建窗口
     namedWindow("【<1>方框滤波】", 1);
     //创建轨迹条
-    createTrackbar("内核值：", "【<1>方框滤波】", &g_nBoxFilterValue, 40, on_BoxFilter);
-    on_BoxFilter(g_nBoxFilterValue, 0);
-    //================================================
+    createTrackbar("内核值：", "【<1>方框滤波】",&g_nBoxFilterValue, 50,on_BoxFilter );
+    on_MeanBlur(g_nBoxFilterValue,0);
+    imshow("【<1>方框滤波】", g_dstImage1);
+    //=====================================================
 
-    //=================【<2>均值滤波】==================
+
+    //=================【<2>均值滤波】==========================
     //创建窗口
     namedWindow("【<2>均值滤波】", 1);
     //创建轨迹条
-    createTrackbar("内核值：", "【<2>均值滤波】",&g_nMeanBlurValue, 40,on_MeanBlur );
+    createTrackbar("内核值：", "【<2>均值滤波】",&g_nMeanBlurValue, 50,on_MeanBlur );
     on_MeanBlur(g_nMeanBlurValue,0);
-    //================================================
+    //======================================================
 
-    //=================【<3>高斯滤波】=====================
+
+    //=================【<3>高斯滤波】===========================
     //创建窗口
     namedWindow("【<3>高斯滤波】", 1);
     //创建轨迹条
-    createTrackbar("内核值：", "【<3>高斯滤波】",&g_nGaussianBlurValue, 40,on_GaussianBlur );
+    createTrackbar("内核值：", "【<3>高斯滤波】",&g_nGaussianBlurValue, 50,on_GaussianBlur );
     on_GaussianBlur(g_nGaussianBlurValue,0);
-    //================================================
+    //=======================================================
+
+
+    //=================【<4>中值滤波】===========================
+    //创建窗口
+    namedWindow("【<4>中值滤波】", 1);
+    //创建轨迹条
+    createTrackbar("参数值：", "【<4>中值滤波】",&g_nMedianBlurValue, 50,on_MedianBlur );
+    on_MedianBlur(g_nMedianBlurValue,0);
+    //=======================================================
+
+
+    //=================【<5>双边滤波】===========================
+    //创建窗口
+    namedWindow("【<5>双边滤波】", 1);
+    //创建轨迹条
+    createTrackbar("参数值：", "【<5>双边滤波】",&g_nBilateralFilterValue, 50,on_BilateralFilter);
+    on_BilateralFilter(g_nBilateralFilterValue,0);
+    //=======================================================
 
 
     //输出一些帮助信息
-    cout<<endl<<"\t运行成功，请调整滚动条观察图像效果~\n\n"
-        <<"\t按下“q”键时，程序退出。\n";
 
-    //按下“q”键时，程序退出
-    while(char(waitKey(1)) != 'q') {}
+    waitKey(0);
 
     return 0;
 }
 
-
 //-----------------------------【on_BoxFilter( )函数】------------------------------------
-//	描述：方框滤波操作的回调函数
+//		描述：方框滤波操作的回调函数
 //-----------------------------------------------------------------------------------------------
 static void on_BoxFilter(int, void *)
 {
@@ -101,39 +121,43 @@ static void on_BoxFilter(int, void *)
     imshow("【<1>方框滤波】", g_dstImage1);
 }
 
-
 //-----------------------------【on_MeanBlur( )函数】------------------------------------
-//	描述：均值滤波操作的回调函数
+//		描述：均值滤波操作的回调函数
 //-----------------------------------------------------------------------------------------------
 static void on_MeanBlur(int, void *)
 {
-    //均值滤波操作
     blur( g_srcImage, g_dstImage2, Size( g_nMeanBlurValue+1, g_nMeanBlurValue+1), Point(-1,-1));
-    //显示窗口
     imshow("【<2>均值滤波】", g_dstImage2);
+
 }
 
-
-//-----------------------------【ContrastAndBright( )函数】------------------------------------
-//	描述：高斯滤波操作的回调函数
+//-----------------------------【on_GaussianBlur( )函数】------------------------------------
+//		描述：高斯滤波操作的回调函数
 //-----------------------------------------------------------------------------------------------
 static void on_GaussianBlur(int, void *)
 {
-    //高斯滤波操作
     GaussianBlur( g_srcImage, g_dstImage3, Size( g_nGaussianBlurValue*2+1, g_nGaussianBlurValue*2+1 ), 0, 0);
-    //显示窗口
     imshow("【<3>高斯滤波】", g_dstImage3);
 }
 
 
-//-----------------------------------【ShowHelpText( )函数】----------------------------------
-//		 描述：输出一些帮助信息
-//----------------------------------------------------------------------------------------------
-void ShowHelpText()
+//-----------------------------【on_MedianBlur( )函数】------------------------------------
+//		描述：中值滤波操作的回调函数
+//-----------------------------------------------------------------------------------------------
+static void on_MedianBlur(int, void *)
 {
-    //输出欢迎信息和OpenCV版本
-    printf("\n\n\t\t\t非常感谢购买《OpenCV3编程入门》一书！\n");
-    printf("\n\n\t\t\t此为本书OpenCV3版的第34个配套示例程序\n");
-    printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION );
-    printf("\n\n  ----------------------------------------------------------------------------\n");
+    medianBlur ( g_srcImage, g_dstImage4, g_nMedianBlurValue*2+1 );
+    imshow("【<4>中值滤波】", g_dstImage4);
 }
+
+
+//-----------------------------【on_BilateralFilter( )函数】------------------------------------
+//		描述：双边滤波操作的回调函数
+//-----------------------------------------------------------------------------------------------
+static void on_BilateralFilter(int, void *)
+{
+    bilateralFilter ( g_srcImage, g_dstImage5, g_nBilateralFilterValue, g_nBilateralFilterValue*2, g_nBilateralFilterValue/2 );
+    imshow("【<5>双边滤波】", g_dstImage5);
+}
+
+
